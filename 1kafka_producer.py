2c31dddcfad4ddd79a6548ee36b24643e3f5cfb9
*TJ <*tjjohn000@gmail.com*>
2021-07-29 01:13:04 +0530

    inital stage

---
+++ 1kafka_producer.py
@@ -0,0 +1,38 @@
+import time
+import requests
+from kafka import KafkaProducer
+from json import dumps
+import json
+
+#***************** Run in Pyspark environemnt and then consume data in the given topic name for testing wheather topic is streaming in server************
+# After confirming by testing, write code for consuumer in scala and write  consumed data to mongodb,mysql
+
+meetup_dot_com_rsvp_stream_api_url = "http://stream.meetup.com/2/rsvps"
+#meetup_dot_com_rsvp_stream_api_url = "https://stream.wikimedia.org/v2/stream/recentchange"
+kafka_topic_name = "wiki-changes"
+kafka_bootstrap_servers = 'localhost:9092'
+
+if __name__ == "__main__":
+    print("Kafka Producer Application Started ... ")
+
+    kafka_producer_obj = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers, value_serializer=lambda x: dumps(x).encode('utf-8'))  #dumps-into key value pair ie  json serelizer
+
+    print("Printing before while loop start ... ")
+    while True:  #infinite loop
+        try:
+            stream_api_response = requests.get(meetup_dot_com_rsvp_stream_api_url, stream=True)
+            if stream_api_response.status_code == 200:
+                for api_response_message in stream_api_response.iter_lines():
+                    print("Message received: ")
+                    print(api_response_message)
+                    print(type(api_response_message))
+
+                    api_response_message = json.loads(api_response_message)
+                    print("Message to be sent: ")
+                    print(api_response_message)
+                    print(type(api_response_message))
+                    kafka_producer_obj.send(kafka_topic_name, api_response_message)
+                    time.sleep(1)
+        except Exception as ex:
+            print('Connection to meetup stream api could not established.')
+    print("Printing after while loop complete.")
---
+++ Kafka+pyspark/consumer+pyspark.py
@@ -0,0 +1,80 @@
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import from_json
+from pyspark.sql.types import StructType, StructField, BooleanType, LongType, IntegerType, StringType
+import time
+
+
+
+kafka_topic_name = "wiki-changes"
+kafka_bootstrap_servers = 'localhost:9092'
+
+
+
+
+if __name__ == "__main__":
+    print("Stream Data Processing Application Started ...")
+    print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+    spark = SparkSession \
+        .builder \
+        .appName("PySpark Structured Streaming with Kafka and Message Format as JSON") \
+        .master("local[*]") \
+        .config("spark.jars", "file:///C://spark_dependency_jars//commons-pool2-2.8.1.jar,file:///C://spark_dependency_jars//spark-sql-kafka-0-10_2.12-3.0.1.jar,file:///C://spark_dependency_jars//kafka-clients-2.6.0.jar,file:///C://spark_dependency_jars//spark-streaming-kafka-0-10-assembly_2.12-3.0.1.jar") \
+        .config("spark.executor.extraClassPath","file:///C://spark_dependency_jars//commons-pool2-2.8.1.jar:file:///C://spark_dependency_jars//spark-sql-kafka-0-10_2.12-3.0.1.jar:file:///C://spark_dependency_jars//kafka-clients-2.6.0.jar:file:///C://spark_dependency_jars//spark-streaming-kafka-0-10-assembly_2.12-3.0.1.jar") \
+        .config("spark.executor.extraLibrary","file:///C://spark_dependency_jars//commons-pool2-2.8.1.jar:file:///C://spark_dependency_jars//spark-sql-kafka-0-10_2.12-3.0.1.jar:file:///C://spark_dependency_jars//kafka-clients-2.6.0.jar:file:///C://spark_dependency_jars//spark-streaming-kafka-0-10-assembly_2.12-3.0.1.jar") \
+        .config("spark.driver.extraClassPath", "file:///C://spark_dependency_jars//commons-pool2-2.8.1.jar:file:///C://spark_dependency_jars//spark-sql-kafka-0-10_2.12-3.0.1.jar:file:///C://spark_dependency_jars//kafka-clients-2.6.0.jar:file:///C://spark_dependency_jars//spark-streaming-kafka-0-10-assembly_2.12-3.0.1.jar") \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    # Construct a streaming DataFrame that reads from topic
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+        .option("subscribe", kafka_topic_name) \
+        .option("startingOffsets", "latest") \
+        .load()
+
+
+    df.printSchema()  # consists of key, value,offset,topic  and  value is the actual message
+
+
+
+    #transforming
+
+    df1 = df.selectExpr("CAST(key AS STRING)", "CAST(value AS string)")
+
+
+
+    new_schema = StructType([
+        StructField("venue", StringType()),
+        StructField("visibility", StringType())
+    ])
+
+
+    df2 = (df1
+           # Sets schema for event data
+           .withColumn("value", from_json("value", new_schema))
+           .withColumn("key", from_json("key", new_schema))
+           )
+
+
+
+    #sinking
+
+    stream1=df2 \
+        .writeStream \
+        .trigger(processingTime='5 seconds') \
+        .outputMode("update") \
+        .option("truncate", "false")\
+        .format("console") \
+        .start()
+    # Note that you have to call start() to actually start the execution of the query
+
+    stream1.awaitTermination()
+
+    print("Stream Data Processing Application Completed.")
